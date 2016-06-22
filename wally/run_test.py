e98a21795cc717e2cbcba69c0b0710c34b267e58
Oleksii Zamiatin <ozamiatin@mirantis.com>
2016-06-22 16:53:15 +0300

    OMG_TEST_NAMES list extracted

--- wally/run_test.py
+++ wally/run_test.py
@@ -43,6 +43,9 @@ TOOL_TYPE_MAPPER = {
 }
 
 
+OMG_TEST_NAMES = ['omg', 'pika', 'rabbit', 'zmq', 'zmq-proxy', 'qpid']
+
+
 logger = logging.getLogger("wally")
 
 
@@ -550,7 +553,7 @@ def console_report_stage(cfg, ctx):
                 rep = "\n\n".join(rep_lst)
             elif tp in ['mysql', 'pgbench'] and data is not None:
                 rep = MysqlTest.format_for_console(data)
-            elif tp in ['omg', 'pika', 'rabbit', 'zmq', 'qpid']:
+            elif tp in OMG_TEST_NAMES:
                 rep = OmgTest.format_for_console(data)
             else:
                 logger.warning("Can't generate text report for " + tp)
@@ -595,7 +598,7 @@ def html_report_stage(cfg, ctx):
                                   cfg.get('comment', ''),
                                   html_rep_fname,
                                   lab_info=ctx.hw_info)
-        if tp in ['omg', 'pika', 'rabbit', 'zmq', 'qpid'] and data is not None:
+        if tp in OMG_TEST_NAMES and data is not None:
             omgbench_report.make_report(cfg.results_dir, cfg.sensor_storage,
                                         html_rep_fname)
 
