1bc3f31659e2a70ad360b12b1022355ea0952dc3
ozamiatin <ozamiatin@mirantis.com>
2016-09-18 23:48:29 +0300

    50 nodes config

--- wally/suits/omgbench/prepare.sh
+++ wally/suits/omgbench/prepare.sh
@@ -19,12 +19,12 @@ cd venv
 virtualenv --no-setuptools .
 
 cd /tmp
-#git clone http://github.com/openstack/oslo.messaging
-git clone https://github.com/ozamiatin/oslo.messaging.git
+git clone http://github.com/openstack/oslo.messaging
+#git clone https://github.com/ozamiatin/oslo.messaging.git
 
 source venv/bin/activate
 pip install setuptools
 pip install eventlet PyYAML oslo.messaging petname redis zmq pika_pool
 cd oslo.messaging
-git fetch https://github.com/ozamiatin/oslo.messaging.git gateway && git checkout FETCH_HEAD
+#git fetch https://github.com/ozamiatin/oslo.messaging.git gateway && git checkout FETCH_HEAD
 pip install .
